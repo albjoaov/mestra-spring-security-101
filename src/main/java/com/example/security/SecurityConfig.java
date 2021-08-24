a7a364194c6b328bd0df39a70b3a186aa8dbd285
João Victor Albuquerque <albjoaov@gmail.com>
2021-08-23 21:55:18 -0300

    add pass encoder

--- src/main/java/com/example/security/SecurityConfig.java
+++ src/main/java/com/example/security/SecurityConfig.java
@@ -1,9 +1,11 @@
 package com.example.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -13,9 +15,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure (AuthenticationManagerBuilder auth) throws Exception {
 		auth
 			.inMemoryAuthentication()
-			.withUser("mesttra").password("123").roles("ADMIN")
+			.withUser("mesttra").password(encoder().encode("123")).roles("ADMIN")
 			.and()
-			.withUser("aluno").password("123").roles("USER");
+			.withUser("aluno").password(encoder().encode("123")).roles("USER");
 	}
 
 	// Authorization config
@@ -28,6 +30,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 				.httpBasic()
 				.and()
 				.formLogin();
+	}
+
+	@Bean
+	public BCryptPasswordEncoder encoder() {
+		return new BCryptPasswordEncoder();
 	}
 	
 
