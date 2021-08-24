8ce5f2acaf386b9d5ff1f90750e97d483284d701
João Victor Albuquerque <albjoaov@gmail.com>
2021-08-24 19:30:07 -0300

    roles

--- src/main/java/com/example/security/endpoints/SimpleController.java
+++ src/main/java/com/example/security/endpoints/SimpleController.java
@@ -1,10 +1,13 @@
 package com.example.security.endpoints;
 
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.security.RolesAllowed;
 import java.util.Map;
 
 @RestController
@@ -16,6 +19,9 @@ public class SimpleController {
 	}
 
 	@GetMapping("/admin")
+//	@Secured ("ROLE_ADMIN")
+//	@PreAuthorize("hasRole('ROLE_USER')")
+//	@RolesAllowed({"ROLE_ADMIN"})
 	public String protectedOla() {
 		return "Olá, ADMIN";
 	}
